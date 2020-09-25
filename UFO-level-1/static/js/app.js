fe17962c44b6ae99bfa453e6f73c706089028d3f
Daniel Carrillo <danielcarrillo@Daniels-MacBook-Pro.local>
2020-09-24 18:06:09 -0600

    update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,8 +1,6 @@
-// from data.js
+// input UFO report values
 var tableData = data;
-// console.log(tableData);
-
-// // input UFO report values
+console.log(tableData);
 var tbody = d3.select("tbody");
 tableData.forEach(function(UFOreport) {
   console.log(UFOreport);
@@ -13,3 +11,27 @@ tableData.forEach(function(UFOreport) {
     cell.text(value);
   });
 });
+
+// event listener to filter data by date input
+function date(filteredUFOreport) {
+    return 
+}
+
+var datetimeFilter = d3.select("#datetime");
+datetimeFilter.on("change", function() {
+    // apply filter to table
+    var filteredData = tabledata.filter()
+  });
+
+  // Create a custom function to return players who made the team
+function madeCut(player) {
+    // return player.madeTeam == true;
+    // A more concise way to express a boolean conditional
+    return player.madeTeam;
+  }
+  
+  // Call the custom function with filter()
+  var playersOnTeam = roster.filter(madeCut);
+  
+  // Display the results
+  console.log(playersOnTeam);
