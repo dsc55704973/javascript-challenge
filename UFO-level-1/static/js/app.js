18a7aee2919ee58708e6f00318d432309e2a0c35
Daniel Carrillo <danielcarrillo@Daniels-MacBook-Pro.local>
2020-09-27 14:00:28 -0600

    update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -22,4 +22,17 @@ function datetimeFilter() {
   d3.event.preventDefault();
   return datetime.datetime == dateInput;
   };
+
+// new filtered table
 var filteredTable = tableData.filter(datatimeFilter);
+console.log(filteredTable);
+var tbody = d3.select("tbody");
+filteredTable.forEach(function(UFOreport) {
+  console.log(UFOreport);
+  var row = tbody.append("tr");
+  Object.entries(UFOreport).forEach(function([key, value]) {
+    console.log(key, value);
+    var cell = row.append("td");
+    cell.text(value);
+  });
+});
