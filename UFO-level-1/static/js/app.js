8f3bd6dd603248b1825d6388543de218739bfc40
Daniel Carrillo <danielcarrillo@Daniels-MacBook-Pro.local>
2020-09-27 16:53:41 -0600

    update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -13,26 +13,32 @@ tableData.forEach(function(UFOreport) {
 });
 
 // event listener to filter data by date input
-var inputElement = d3.select("#datetime");
-var dateInput = inputElement.property("value");
+
 var filterButton = d3.select("#filter-btn");
 filterButton.on("click", datetimeFilter);
 console.log(dateInput)
 
-function datetimeFilter() {
-  console.log(dateInput);
-  // d3.event.preventDefault();
-  // new filtered table
-  var filteredTable = tableData.filter(datetime.datetime == dateInput);
-  console.log(filteredTable);
-  var tbody = d3.select("tbody");
-  filteredTable.forEach(function(UFOreport) {
-    console.log(UFOreport);
-    var row = tbody.append("tr");
-    Object.entries(UFOreport).forEach(function([key, value]) {
-      console.log(key, value);
-      var cell = row.append("td");
-      cell.text(value);
-    });
-  });
-};
+if (dateInput) {
+  // filter here
+  function datetimeFilter() {
+    var inputElement = d3.select("#datetime");
+    var dateInput = inputElement.property("value");
+    console.log(dateInput);
+    // d3.event.preventDefault();
+    // new filtered table
+    var filteredTable = tableData.filter(function(row) {
+      return row.datetime == dateInput});
+      console.log(filteredTable);
+      var tbody = d3.select("tbody");
+      filteredTable.forEach(function(UFOreport) {
+        console.log(UFOreport);
+        var row = tbody.append("tr");
+        Object.entries(UFOreport).forEach(function([key, value]) {
+          console.log(key, value);
+          var cell = row.append("td");
+          cell.text(value);
+        });
+      });
+    };
+  };
+ 
