1736a7c745ee7c1317f8ec9485dd8aa556ec48d4
Daniel Carrillo <danielcarrillo@Daniels-MacBook-Pro.local>
2020-09-27 15:30:34 -0600

    update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -17,22 +17,22 @@ var inputElement = d3.select("#datetime");
 var dateInput = inputElement.property("value");
 var filterButton = d3.select("#filter-btn");
 filterButton.on("click", datetimeFilter);
+console.log(dateInput)
 
 function datetimeFilter() {
-  d3.event.preventDefault();
-  return datetime.datetime == dateInput;
-  };
-
-// new filtered table
-var filteredTable = tableData.filter(datatimeFilter);
-console.log(filteredTable);
-var tbody = d3.select("tbody");
-filteredTable.forEach(function(UFOreport) {
-  console.log(UFOreport);
-  var row = tbody.append("tr");
-  Object.entries(UFOreport).forEach(function([key, value]) {
-    console.log(key, value);
-    var cell = row.append("td");
-    cell.text(value);
+  console.log(dateInput);
+  // d3.event.preventDefault();
+  // new filtered table
+  var filteredTable = tableData.filter(datetime.datetime == dateInput);
+  console.log(filteredTable);
+  var tbody = d3.select("tbody");
+  filteredTable.forEach(function(UFOreport) {
+    console.log(UFOreport);
+    var row = tbody.append("tr");
+    Object.entries(UFOreport).forEach(function([key, value]) {
+      console.log(key, value);
+      var cell = row.append("td");
+      cell.text(value);
+    });
   });
-});
+};
